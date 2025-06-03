84df9073f95d758426150c60a4d05c58f5081b92
Pratik <pratikid@gmail.com>
2025-06-04 00:39:42 +0530

    feat: register IndividualPolicy for Individual model in AuthServiceProvider - Added IndividualPolicy to AuthServiceProvider to enable authorization for Individual model actions.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\TimelineEvent;
 use App\Policies\TimelineEventPolicy;
+use App\Models\Individual;
+use App\Policies\IndividualPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -11,6 +13,7 @@ class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         TimelineEvent::class => TimelineEventPolicy::class,
+        Individual::class => IndividualPolicy::class,
     ];
 
     public function boot()
